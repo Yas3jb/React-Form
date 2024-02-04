b170fb8d84477579a7d2e4433cb68359a74abc14
yas3jb <yasmu213@gmail.com>
2024-02-04 17:34:12 -0500

    Form Validation added

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -7,22 +7,42 @@ export default function SignUpForm({ setToken }) {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
-    try {
-      const response = await fetch(
-        "https://fsa-jwt-practice.herokuapp.com/signup",
-        {
-          method: "POST",
-          body: JSON.stringify({ username, password }),
-        }
-      );
-      const result = await response.json();
-      setToken(result.token);
-      console.log(result);
-    } catch (error) {
-      setError(error.message);
+    if (formValidation()) {
+      try {
+        const response = await fetch(
+          "https://fsa-jwt-practice.herokuapp.com/signup",
+          {
+            method: "POST",
+            body: JSON.stringify({ username: username, password: password }),
+          }
+        );
+        const result = await response.json();
+        setToken(result.token);
+        console.log(result);
+      } catch (error) {
+        setError(error.message);
+      }
     }
   }
+
+  // Form validation
+
+  function formValidation() {
+    let message = "";
+    if (!username) {
+      message += "Please enter a username. ";
+    } else if (username.length > 8) {
+      message += "Username must be at least 8 characters long. ";
+    }
+    if (message) {
+      setError(message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
   return (
     <div>
       <h2>Sign Up!</h2>
